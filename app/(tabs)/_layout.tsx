243b660066519bc099dbb63b28c5db914e5f9dcb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 11:36:52 +0300

    Added some lines of code to properly configure the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,26 @@ import React from 'react'
 const TabsLayout = () => {
 	return (
 		<Tabs>
+			<Tabs.Screen
+				name='index'
+				options={{ tabBarLabel: 'Home', headerTitle: 'Home' }}
+			/>
+			<Tabs.Screen
+				name='inbox'
+				options={{ tabBarLabel: 'Inbox', headerTitle: 'Inbox' }}
+			/>
+			<Tabs.Screen
+				name='explore'
+				options={{ tabBarLabel: 'Explore', headerTitle: 'Explore' }}
+			/>
+			<Tabs.Screen
+				name='profile'
+				options={{ tabBarLabel: 'Profile', headerTitle: 'Profile' }}
+			/>
+			<Tabs.Screen
+				name='wishlists'
+				options={{ tabBarLabel: 'WishLists', headerTitle: 'WishLists' }}
+			/>
 		</Tabs>
 	)
 }
