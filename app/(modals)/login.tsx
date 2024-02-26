d9fd15d594ae23df4059f5a730d017cb56e638d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 14:30:18 +0300

    Added some components to the Login component

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,14 +1,27 @@
 import { defaultStyles } from '@/constants/Styles'
 import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View
+} from 'react-native'
 
 const LoginModal = () => {
 	useWarmUpBrowser()
 
 	return (
 		<View style={styles.container}>
-			<TextInput autoCapitalize='none' placeholder='Email' style={[defaultStyles.inputField]} />
+			<TextInput
+				autoCapitalize='none'
+				placeholder='Email'
+				style={[defaultStyles.inputField, { marginBottom: 30 }]}
+			/>
+			<TouchableOpacity style={defaultStyles.btn}>
+				<Text style={defaultStyles.btnText}>Continue</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
