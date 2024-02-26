d3b291f5492f9cc3c9c30c70bace0c162c28e01e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 12:58:18 +0300

    Clerk authentication shenanigans

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,30 @@ import { useFonts } from 'expo-font'
 import { Stack, useRouter } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
+import * as SecureStore from 'expo-secure-store'
 
 import { Ionicons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo'
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key)
+		} catch (error) {
+			return null
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value)
+		} catch (error) {
+			return
+		}
+	}
+}
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -22,7 +43,7 @@ SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
 	const [loaded, error] = useFonts({
-		'mon': require('../assets/fonts/Montserrat-Regular.ttf'),
+		mon: require('../assets/fonts/Montserrat-Regular.ttf'),
 		'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
 		'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
 		...FontAwesome.font
@@ -43,11 +64,25 @@ export default function RootLayout() {
 		return null
 	}
 
-	return <RootLayoutNav />
+	return (
+		<ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY!}
+			tokenCache={tokenCache}
+		>
+			<RootLayoutNav />
+		</ClerkProvider>
+	)
 }
 
 function RootLayoutNav() {
 	const router = useRouter()
+	const { isLoaded, isSignedIn } = useAuth()
+
+	useEffect(() => {
+		if (isLoaded && !isSignedIn) {
+			router.push('/(modals)/login')
+		}
+	}, [isLoaded])
 
 	return (
 		<Stack>
@@ -74,8 +109,8 @@ function RootLayoutNav() {
 				options={{
 					presentation: 'transparentModal',
 					animation: 'slide_from_bottom',
-          headerTitle: 'Booking',
-          headerTitleStyle: {
+					headerTitle: 'Booking',
+					headerTitleStyle: {
 						fontFamily: 'mon-sb'
 					},
 					headerLeft: () => (
