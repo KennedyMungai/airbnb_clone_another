802acdb045a7e932d40ab64a368d674926043dbd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 12:05:18 +0300

    Created the page to render the info for a single listing

---
+++ app/listing/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListingDetailsPage = () => {
+	return (
+		<View>
+			<Text>ListingDetailsPage</Text>
+		</View>
+	)
+}
+
+export default ListingDetailsPage
