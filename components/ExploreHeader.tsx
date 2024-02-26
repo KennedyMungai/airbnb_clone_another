e8167bcaa999bfe18a42db91ecad199a1d5ce2b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 15:51:02 +0300

    Added the Categories array of objects

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,6 +1,38 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const categories = [
+    {
+      name: 'Tiny homes',
+      icon: 'home',
+    },
+    {
+      name: 'Cabins',
+      icon: 'house-siding',
+    },
+    {
+      name: 'Trending',
+      icon: 'local-fire-department',
+    },
+    {
+      name: 'Play',
+      icon: 'videogame-asset',
+    },
+    {
+      name: 'City',
+      icon: 'apartment',
+    },
+    {
+      name: 'Beachfront',
+      icon: 'beach-access',
+    },
+    {
+      name: 'Countryside',
+      icon: 'nature-people',
+    },
+  ];
+  
+
 const ExploreHeader = () => {
 	return (
 		<View>
