4a8b28a2224448ffc011991214e5ce5664c1713c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 14:36:43 +0300

    Added some markup items and styled them

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,3 +1,4 @@
+import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/Styles'
 import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 import React from 'react'
@@ -22,6 +23,23 @@ const LoginModal = () => {
 			<TouchableOpacity style={defaultStyles.btn}>
 				<Text style={defaultStyles.btnText}>Continue</Text>
 			</TouchableOpacity>
+			<View style={styles.separatorView}>
+				<View
+					style={{
+						borderBottomColor: '#000',
+						borderBottomWidth: StyleSheet.hairlineWidth,
+						flex: 1
+					}}
+				/>
+				<Text style={styles.separator}>Or</Text>
+				<View
+					style={{
+						borderBottomColor: '#000',
+						borderBottomWidth: StyleSheet.hairlineWidth,
+						flex: 1
+					}}
+				/>
+			</View>
 		</View>
 	)
 }
@@ -33,5 +51,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 26,
 		backgroundColor: '#fff'
+	},
+	separatorView: {
+		flexDirection: 'row',
+		gap: 10,
+		alignItems: 'center',
+        marginVertical: 30
+	},
+	separator: {
+		fontFamily: 'mon-sb',
+		color: Colors.grey
 	}
 })
