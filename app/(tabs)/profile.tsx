9fcc0cb3b500f947bd8414007b9cec8104907e50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 13:07:54 +0300

    Added the functionality to sign out and sign in in the profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { useAuth } from '@clerk/clerk-react'
+import { Link } from 'expo-router'
 import React from 'react'
+import { Button, Text, View } from 'react-native'
 
 const Profile = () => {
+	const { signOut, isSignedIn } = useAuth()
+
 	return (
 		<View>
-			<Text>Profile</Text>
+			<Button title='Log Out' onPress={() => signOut()} />
+			{!isSignedIn && (
+				<Link href={'/(modals)/login'}>
+					<Text>Login</Text>
+				</Link>
+			)}
 		</View>
 	)
 }
