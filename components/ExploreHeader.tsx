1fba243b4d5d4797ccf823d5a1ed51212606a4cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 15:59:20 +0300

    Added the components inside a SafeAreaView component

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,44 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const categories = [
-    {
-      name: 'Tiny homes',
-      icon: 'home',
-    },
-    {
-      name: 'Cabins',
-      icon: 'house-siding',
-    },
-    {
-      name: 'Trending',
-      icon: 'local-fire-department',
-    },
-    {
-      name: 'Play',
-      icon: 'videogame-asset',
-    },
-    {
-      name: 'City',
-      icon: 'apartment',
-    },
-    {
-      name: 'Beachfront',
-      icon: 'beach-access',
-    },
-    {
-      name: 'Countryside',
-      icon: 'nature-people',
-    },
-  ];
-  
+	{
+		name: 'Tiny homes',
+		icon: 'home'
+	},
+	{
+		name: 'Cabins',
+		icon: 'house-siding'
+	},
+	{
+		name: 'Trending',
+		icon: 'local-fire-department'
+	},
+	{
+		name: 'Play',
+		icon: 'videogame-asset'
+	},
+	{
+		name: 'City',
+		icon: 'apartment'
+	},
+	{
+		name: 'Beachfront',
+		icon: 'beach-access'
+	},
+	{
+		name: 'Countryside',
+		icon: 'nature-people'
+	}
+]
 
 const ExploreHeader = () => {
 	return (
-		<View>
-			<Text>ExploreHeader</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1 }}>
+			<View>
+				<View style={styles.container}></View>
+			</View>
+		</SafeAreaView>
 	)
 }
 
 export default ExploreHeader
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#FFF'
+	}
+})
