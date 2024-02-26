4b22a3aaeb426929e9179dbc6f73a100bd70e82f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-26 11:24:22 +0300

    Some more fonts shenanigans

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,9 @@ SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
 	const [loaded, error] = useFonts({
-		SpaceMono: require('../assets/fonts/Montserrat-Regular.ttf'),
+		'mon': require('../assets/fonts/Montserrat-Regular.ttf'),
+		'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+		'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
 		...FontAwesome.font
 	})
 
