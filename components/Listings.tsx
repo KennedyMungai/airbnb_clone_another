34882e88deefb3ef0903147b5fc61c5c0d5e3a74
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-27 16:14:28 +0300

    Made the Listings data change when the category changes

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Listings = () => {
+type Props = {
+	listings: any[]
+	category: string
+}
+
+const Listings = ({ category, listings }: Props) => {
+	useEffect(() => {
+		console.log('RELOAD LISTINGS')
+	}, [category])
+
 	return (
 		<View>
 			<Text>Listings</Text>
