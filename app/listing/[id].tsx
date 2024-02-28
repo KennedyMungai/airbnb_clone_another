c714cc5e6d1aaaf4ecec4c875069ffa9e6c1b34a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-28 15:41:25 +0300

    Styled the page

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,16 +1,25 @@
+import listingsData from '@/assets/data/airbnb-listings.json'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const ListingDetailsPage = () => {
 	const { id } = useLocalSearchParams<{ id: string }>()
+	const listing = (listingsData as any[]).find((item) => item.id === id)
 
 	return (
-		<View>
-            <Stack.Screen options={{headerTitle: id}} />
+		<View style={styles.container}>
+			<Stack.Screen options={{ headerTitle: id }} />
 			<Text>ListingDetailsPage</Text>
 		</View>
 	)
 }
 
 export default ListingDetailsPage
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	}
+})
