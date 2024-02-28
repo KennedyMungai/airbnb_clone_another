0508e50c57c7d4e1203aac6884b557722b00a168
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-28 14:42:08 +0300

    Added the code to map out the data in the airbnb listings json file

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,21 +1,60 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { defaultStyles } from '@/constants/Styles'
+import { Link } from 'expo-router'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+	FlatList,
+	Image,
+	ListRenderItem,
+	StyleSheet,
+	TouchableOpacity,
+	View
+} from 'react-native'
 
 type Props = {
 	listings: any[]
 	category: string
 }
 
-const Listings = ({ category, listings }: Props) => {
+const Listings = ({ category, listings: items }: Props) => {
+	const [loading, setLoading] = useState(false)
+	const listRef = useRef<FlatList>(null)
+
 	useEffect(() => {
-		console.log('RELOAD LISTINGS')
+		console.log('RELOAD LISTINGS', items.length)
+		setLoading(true)
+
+		setTimeout(() => {
+			setLoading(false)
+		}, 300)
 	}, [category])
 
+	const renderRow: ListRenderItem<any> = ({ item }) => {
+		return (
+			<Link href={`/listing/${item.id}`}>
+				<TouchableOpacity>
+					<View style={styles.listing}>
+						<Image source={{ uri: item.medium_url }} />
+					</View>
+				</TouchableOpacity>
+			</Link>
+		)
+	}
+
 	return (
-		<View>
-			<Text>Listings</Text>
+		<View style={defaultStyles.container}>
+			<FlatList
+				data={loading ? [] : items}
+				ref={listRef}
+				renderItem={renderRow}
+			/>
 		</View>
 	)
 }
 
 export default Listings
+
+const styles = StyleSheet.create({
+	listing: {
+		padding: 16
+	}
+})
