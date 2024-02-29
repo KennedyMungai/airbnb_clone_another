d2f62a0f8b1b53646147b6b4702b49a5655596ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-29 13:01:20 +0300

    Added some styling for the map button

--- components/ListingsBottomSheet.tsx
+++ components/ListingsBottomSheet.tsx
@@ -1,8 +1,10 @@
 import Colors from '@/constants/Colors'
 import { Listing } from '@/interfaces/listing'
+import { Ionicons } from '@expo/vector-icons'
 import BottomSheet from '@gorhom/bottom-sheet'
 import React, { useMemo, useRef } from 'react'
-import { View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import Listings from './Listings'
 
 type Props = {
@@ -14,6 +16,8 @@ const ListingsBottomSheet = ({ category, listings }: Props) => {
 	const bottomSheetRef = useRef<BottomSheet>(null)
 	const snapPoints = useMemo(() => ['10%', '100%'], [])
 
+	const showMap = () => {}
+
 	return (
 		<BottomSheet
 			ref={bottomSheetRef}
@@ -24,9 +28,35 @@ const ListingsBottomSheet = ({ category, listings }: Props) => {
 		>
 			<View style={{ flex: 1 }}>
 				<Listings listings={listings} category={category} />
+				<View style={styles.absoluteButton}>
+					<TouchableOpacity onPress={showMap} style={styles.btn}>
+						<Text style={{ fontFamily: 'mon-sb', color: 'white' }}>
+							Map
+						</Text>
+						<Ionicons name='map' size={20} color='white' />
+					</TouchableOpacity>
+				</View>
 			</View>
 		</BottomSheet>
 	)
 }
 
 export default ListingsBottomSheet
+
+const styles = StyleSheet.create({
+	absoluteButton: {
+		position: 'absolute',
+		bottom: 30,
+		width: '100%',
+		alignItems: 'center'
+	},
+	btn: {
+		backgroundColor: Colors.dark,
+		padding: 16,
+		height: 50,
+		alignItems: 'center',
+		flexDirection: 'row',
+		borderRadius: 30,
+		gap: 8
+	}
+})
