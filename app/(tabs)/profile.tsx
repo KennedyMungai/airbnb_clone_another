b8d3bc33a6a8b52d31d7e6775db183ad11e8468b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-29 15:33:35 +0300

    Added a signOut button

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,11 @@
+import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/Styles'
 import { useUser } from '@clerk/clerk-expo'
 import { useAuth } from '@clerk/clerk-react'
 import { Ionicons } from '@expo/vector-icons'
 import { Link } from 'expo-router'
 import React, { useEffect, useState } from 'react'
-import { Button, Text, View, StyleSheet, SafeAreaView } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 const Profile = () => {
 	const { signOut, isSignedIn } = useAuth()
@@ -33,6 +34,15 @@ const Profile = () => {
 				<Text style={styles.headerText}>Profile</Text>
 				<Ionicons name='notifications-outline' size={26} />
 			</View>
+
+			{isSignedIn && (
+				<Button
+					title='Log Out'
+					onPress={() => signOut()}
+					color={Colors.dark}
+				/>
+			)}
+
 			<Button title='Log Out' onPress={() => signOut()} />
 			{!isSignedIn && (
 				<Link href={'/(modals)/login'}>
