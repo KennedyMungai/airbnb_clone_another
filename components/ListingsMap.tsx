54b99af3faf7d5192edb28afcc41ec674f1b0b49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-29 10:00:13 +0300

    Created the component for the map of the listings

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,14 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import MapView from 'react-native-maps'
 
+type Props = {
+	listings: any[]
+}
 
-const ListingsMap = () => {
+const ListingsMap = ({ listings }: Props) => {
 	return (
-		<View>
-			<Text>ListingsMap</Text>
+		<View style={styles.container}>
+			<MapView style={styles.map} />
 		</View>
 	)
 }
 
 export default ListingsMap
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	map: {
+		width: '100%',
+		height: '100%'
+	}
+})
