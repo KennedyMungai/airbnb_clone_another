eb7a7e73bea36cdfd4dc6c62ed9fc8492192de2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-29 09:52:51 +0300

    Created a template for the ListingsMap component

---
+++ components/ListingsMap.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps'
+
+
+const ListingsMap = () => {
+	return (
+		<View>
+			<Text>ListingsMap</Text>
+		</View>
+	)
+}
+
+export default ListingsMap
