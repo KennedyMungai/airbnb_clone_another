16adf1d76c7b37df676aca85c2fa8a1fcc55fc2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-29 10:06:39 +0300

    Changed the maps provider to google

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { defaultStyles } from '@/constants/Styles'
 
 type Props = {
@@ -10,7 +10,12 @@ type Props = {
 const ListingsMap = ({ listings }: Props) => {
 	return (
 		<View style={defaultStyles.container}>
-			<MapView style={styles.map} showsUserLocation showsMyLocationButton />
+			<MapView
+				style={styles.map}
+				provider={PROVIDER_GOOGLE}
+				showsUserLocation
+				showsMyLocationButton
+			/>
 		</View>
 	)
 }
