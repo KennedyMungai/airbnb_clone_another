8aec05ba5e7b6cf64131395ff1f6089c0079465d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 10:06:07 +0300

    Added some styles to the Main vie of the ModalHeaderText

--- components/ModalHeaderText.tsx
+++ components/ModalHeaderText.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ModalHeaderText = () => {
 	return (
-		<View>
+		<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
 			<Text>ModalHeaderText</Text>
 		</View>
 	)
