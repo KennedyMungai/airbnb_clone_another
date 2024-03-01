60fae8d9d8db65ab88c8b14f9bc090c134631e03
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 10:17:37 +0300

    Added some styles for the component

--- components/ModalHeaderText.tsx
+++ components/ModalHeaderText.tsx
@@ -5,9 +5,13 @@ const ModalHeaderText = () => {
 	const [active, setActive] = useState(0)
 
 	return (
-		<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+		<View
+			style={{ flexDirection: 'row', justifyContent: 'center', gap: 10 }}
+		>
 			<TouchableOpacity onPress={() => setActive(0)}>
-				<Text>Stays</Text>
+				<Text style={{ fontFamily: 'mon-sb', fontSize: 18 }}>
+					Stays
+				</Text>
 			</TouchableOpacity>
 			<TouchableOpacity onPress={() => setActive(1)}>
 				<Text>Experiences</Text>
