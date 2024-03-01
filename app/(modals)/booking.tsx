9ad1d3ba3afb401828ef01d3c1b9e7088ed78ec0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 11:28:50 +0300

    Added a borderWidth and gave the active image a border color of the predefined grey

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -284,6 +284,7 @@ const styles = StyleSheet.create({
 		width: 100,
 		height: 100,
 		borderRadius: 10,
-		borderWidth: StyleSheet.hairlineWidth
+		borderWidth: 2,
+		borderColor: Colors.grey
 	}
 })
