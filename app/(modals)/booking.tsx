2f929d163b63e5cb628da015a92df49cfb3f4dba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 10:04:30 +0300

    Rendered out the ModalHeaderText component and the Header of the BookingModal

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,3 +1,4 @@
+import ModalHeaderText from '@/components/ModalHeaderText'
 import { Stack } from 'expo-router'
 import React from 'react'
 import { Text, View } from 'react-native'
@@ -5,7 +6,12 @@ import { Text, View } from 'react-native'
 const BookingModal = () => {
 	return (
 		<View>
-			<Stack.Screen options={{headerTransparent: true}} />
+			<Stack.Screen
+				options={{
+					headerTransparent: true,
+					headerTitle: () => <ModalHeaderText />
+				}}
+			/>
 			<Text>BookingModal</Text>
 		</View>
 	)
